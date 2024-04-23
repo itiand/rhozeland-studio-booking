6c6e49553c8cbef99f5631179845ec0540ec2bee
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-04-23 15:01:16 -0600

    building card

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
-import { FaBeer } from "react-icons/fa";
+import { HiArrowLongRight } from "react-icons/hi2";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-      <section>
-        <h1>Work With Us Today</h1>
+      <section class="px-2.5">
+        <h1 class="">Work With Us Today</h1>
         <p>
           Check out our availability and book the date and time that works for
           you.
         </p>
         <div id="services">
-          <div class="svs-card">
-            <img src="https://picsum.photos/400/300" alt="" />
-            <div>
+          <div class="svs-card bg-slate-200">
+            <img src="https://picsum.photos/400/300" alt="" class="w-full" />
+            <div class="flex justify-between items-center">
               <p class="svs-title">Session</p>
               <div>
-                <FaBeer></FaBeer>
+                <HiArrowLongRight
+                  style={{ fontSize: "27px" }}
+                ></HiArrowLongRight>
               </div>
             </div>
           </div>
