60915e859a479b4d2111cddcd230b83b8061db87
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-04-22 18:52:15 -0600

    start skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hola</h1>
+      <section>
+        <h1>Work With Us Today</h1>
+        <p>
+          Check out our availability and book the date and time that works for
+          you.
+        </p>
+        <div id="services">
+          <div class="svs-card">
+            <img src="https://picsum.photos/400/300" alt="" />
+            <div></div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
