13e4a31be3d9530962173791b12b1aa20a9a5272
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-04-23 09:43:24 -0600

    set up tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
       <section>
         <h1>Work With Us Today</h1>
         <p>
@@ -13,7 +14,12 @@ function App() {
         <div id="services">
           <div class="svs-card">
             <img src="https://picsum.photos/400/300" alt="" />
-            <div></div>
+            <div>
+              <p class="svs-title"></p>
+              <div>
+                <i class="arrow"></i>
+              </div>
+            </div>
           </div>
         </div>
       </section>
