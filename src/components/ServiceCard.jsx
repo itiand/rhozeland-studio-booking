92e8f431cca8ad7813118310c099dfff9ada5025
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-04-28 23:11:48 -0600

    implement dropdown show on service card component

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,10 +1,20 @@
 import { HiArrowLongRight } from "react-icons/hi2";
 import podcastImage from "../assets/podcast.jpg";
+import { useState } from "react";
 
 const ServiceCard = ({ serviceName }) => {
+  const [isDetailsVisible, setIsDetailsVisible] = useState(false);
+
+  const toggleDetailsVisibility = () => {
+    setIsDetailsVisible(!isDetailsVisible);
+  };
+
   return (
     <>
-      <article className="svs-card bg-slate-200 text-orange-700 mlandscape:text-blue-800 tablet:text-purple-900 desktop:text-green-900">
+      <article
+        className="svs-card bg-slate-200 text-orange-700 mlandscape:text-blue-800 tablet:text-purple-900 desktop:text-green-900"
+        onClick={toggleDetailsVisibility}
+      >
         <img
           src="https://picsum.photos/400/300"
           alt="Service preview"
@@ -16,8 +26,11 @@ const ServiceCard = ({ serviceName }) => {
             <HiArrowLongRight style={{ fontSize: "27px" }}></HiArrowLongRight>
           </button>
         </footer>
-        <div className="hidden">
-          <img src={podcastImage} alt="Service preview" className="w-full" />
+
+        <div ///hidden portion
+          className={`details-section ${isDetailsVisible ? "expanded" : ""}`}
+        >
+          <img src={podcastImage} alt="Service preview" className="" />
         </div>
       </article>
     </>
