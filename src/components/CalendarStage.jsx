5f29b9b82fd638d124228e373622fa8433722774
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-05-31 14:15:37 -0400

    populating calendar with sample bookings

--- src/components/CalendarStage.jsx
+++ src/components/CalendarStage.jsx
@@ -11,6 +11,18 @@ import {
 } from "@syncfusion/ej2-react-schedule";
 
 const CalendarStage = ({ appointmentType, specialist }) => {
+  //sample bookings
+  const data = [
+    {
+      Id: 1,
+      Subject: "Meeting - 1",
+      StartTime: new Date(2024, 6, 2, 10, 0),
+      EndTime: new Date(2024, 6, 2, 12, 30),
+      IsAllDay: false,
+    },
+  ];
+  const eventSettings = { dataSource: data };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -18,7 +30,11 @@ const CalendarStage = ({ appointmentType, specialist }) => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
       className="calendar-stage p-4 bg-gray-900 text-white"
-    ></motion.div>
+    >
+      <ScheduleComponent eventSettings={eventSettings}>
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      </ScheduleComponent>
+    </motion.div>
   );
 };
 
