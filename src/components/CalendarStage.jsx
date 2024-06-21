09ef11508b7492bb3db5e6d1676c28954f3265e8
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-06-21 15:02:38 -0400

    fix slight date discrepancy - removving newDate method

--- src/components/CalendarStage.jsx
+++ src/components/CalendarStage.jsx
@@ -13,6 +13,7 @@ import {
   ViewsDirective,
   DragAndDrop,
   TimelineViews,
+  Timezone,
 } from "@syncfusion/ej2-react-schedule";
 
 import { DataManager, WebApiAdaptor } from "@syncfusion/ej2-data";
@@ -81,44 +82,9 @@ const CalendarStage = ({ appointmentType, specialist }) => {
       EndTime: new Date(2024, 5, 16, 15, 0),
       IsAllDay: false,
     },
-    {
-      Id: 5,
-      Subject: "Meeting - 5",
-      StartTime: new Date(2024, 5, 17, 16, 0),
-      EndTime: new Date(2024, 5, 17, 17, 0),
-      IsAllDay: false,
-    },
-    {
-      Id: 6,
-      Subject: "Meeting - 6",
-      StartTime: new Date(2024, 5, 18, 13, 0),
-      EndTime: new Date(2024, 5, 18, 14, 0),
-      IsAllDay: false,
-    },
-    {
-      Id: 7,
-      Subject: "Meeting - 7",
-      StartTime: new Date(2024, 5, 19, 10, 0),
-      EndTime: new Date(2024, 5, 19, 11, 0),
-      IsAllDay: false,
-    },
-    {
-      Id: 8,
-      Subject: "Meeting - 8",
-      StartTime: new Date(2024, 5, 20, 15, 0),
-      EndTime: new Date(2024, 5, 20, 16, 0),
-      IsAllDay: false,
-    },
-    {
-      Id: 9,
-      Subject: "Meeting - 9",
-      StartTime: new Date(2024, 5, 21, 10, 0),
-      EndTime: new Date(2024, 5, 21, 11, 0),
-      IsAllDay: false,
-    },
   ];
 
-  ////remote data
+  ////remote data set up
   let calendarId =
     "e133f66d097f96376e7ba4f32278b6e516e45d051648a23d203d1cbfb866fa6b@group.calendar.google.com";
   const apiAccessKey = "AIzaSyDOp-Mv-voooD18ddspABgZ-I4qcrwMlnw";
@@ -128,31 +94,37 @@ const CalendarStage = ({ appointmentType, specialist }) => {
     adaptor: new WebApiAdaptor(),
     crossDomain: true,
   });
+  ////end remote data set up
 
   const onDataBinding = (e) => {
     // Extract items from the event result
     let items = e.result.items;
     let schedulerData = [];
+    let timezone = new Timezone();
 
     // Check if there are items to process
     if (items.length > 0) {
       for (let event of items) {
-        // Determine if the event is all day
         let isAllDay = !event.start.dateTime;
         let start = event.start.dateTime || event.start.date;
         let end = event.end.dateTime || event.end.date;
+
+        if (event.summary === `All Day`) {
+          console.log(`start`, start);
+          console.log(`end`, end);
+        }
 
         // Push formatted event data to schedulerData
         schedulerData.push({
           Id: event.id,
           Subject: event.summary,
-          StartTime: new Date(start),
-          EndTime: new Date(end),
+          StartTime: start,
+          EndTime: end,
           IsAllDay: isAllDay,
         });
+        // console.log(`${event.summary} pushed`, schedulerData);
       }
     }
-
     // Update the event settings with the formatted data
     e.result = schedulerData;
   };
@@ -171,9 +143,7 @@ const CalendarStage = ({ appointmentType, specialist }) => {
       <button style={{ backgroundColor: "red" }} onClick={refreshEvents}>
         Refresh Events
       </button>
-      <button onClick={() => console.log("eventSettings", eventSettings)}>
-        Get Data
-      </button>
+      <button onClick={() => console.log("Timezone", Timezone)}>Debug</button>
       {/* SCHEDULE COMPONENT */}
       <ScheduleComponent
         eventSettings={eventSettings}
