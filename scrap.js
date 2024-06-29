6cba5097d48c0ef51dc010d47dc9ee32c38605ec
Christian Delos Reyes <christian.dreyes@gmail.com>
2024-06-29 12:58:04 -0400

    Remove scrap.js from tracking

--- scrap.js
+++
@@ -1,9 +0,0 @@
-//fetch all the appointments from the api
-//populate then on the calendar
-
-//CALENDAR
-//users can create their own appointment
-//the cannot edit others
-//
-
-//make calendar time between 9 to 11pm.
